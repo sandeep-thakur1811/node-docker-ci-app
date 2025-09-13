41f4bcb7ca299545ee15001331409e6bbe412809
Ubuntu <ubuntu@ip-172-31-36-167.ap-south-1.compute.internal>
2025-09-13 18:20:10 +0000

    Initial commit

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM node:18
 WORKDIR /app
 
 COPY package*.json ./
+
 RUN npm install
 
 COPY . .
@@ -10,3 +11,4 @@ COPY . .
 EXPOSE 3000
 
 CMD ["npm", "start"]
+
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('🚀 Node.js app is running inside Docker with Nginx & Jenkins!');
+app.get("/", (req, res) => {
+  res.send("🚀 Node.js app is running inside Docker with Nginx & Jenkins!");
 });
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
+
